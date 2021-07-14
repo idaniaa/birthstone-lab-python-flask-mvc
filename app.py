1a6797bc0323c6784afb13b4a5a13d81611fa006
Idania <idaniajimenez24@gmail.com>
2021-07-14 18:22:16 +0000

    idaniaj1998

--- app.py
+++ app.py
@@ -6,12 +6,40 @@ from flask import request
 import model
 
 
+
 # -- Initialization section --
 app = Flask(__name__)
 
 
 # -- Routes section --
 @app.route('/')
-@app.route('/index')
+@app.route('/index') 
 def index():
-    return render_template('index.html')
+   
+    return render_template("index.html")
+
+@app.route('/result',methods=["GET","POST"])
+def result(): 
+    print(request.form["stone"])
+    user_choice=request.form["stone"]
+    
+    stone= {
+        "January":"Garnet",
+        "Febuary":"Anethyst",
+        "March":"Aquamarine",
+        "April":"Diamond",
+        "May":"Emerald",
+        "June":"Pearl",
+        "July":"Ruby",
+        "August":"Peridot",
+        "September":"Sapphire",
+        "October":"Opal",
+        "November":"Citrine",
+        "December":"Topaz"}
+    try:
+        message=stone[user_choice]
+
+    except:
+        message=("error, please try another month")
+        
+    return render_template("results.html", message=message)
---
+++ model.py
@@ -0,0 +1,28 @@
+def stone_choice_message(user_choice):
+    if user_choice == "January":
+        return "Garnet!"
+    elif user_choice =="Febuary":
+        return "Amethyst"
+    elif user_choice =="March":
+        return "Aquamarine"
+    elif user_choice =="April":
+        return "Diamond"
+    elif user_choice =="May":
+        return "Emerald"
+    elif user_choice =="June":
+        return "Pearl"
+    elif user_choice =="July":
+        return "Ruby"
+    elif user_choice =="August":
+        return "Peridot"
+    elif user_choice =="September":
+        return "Sapphire"
+    elif user_choice =="October":
+        return "Opal"
+    elif user_choice =="November":
+        return "Citrine"
+    elif user_choice =="December":
+        return "Topaz"
+    else:
+        return "Invalid Imput"
+    
